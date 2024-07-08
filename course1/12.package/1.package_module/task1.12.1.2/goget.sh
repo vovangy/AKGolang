9af2e7b216ba7643bae8d193de56fbd04e912e69
ganger <malenko.2003@mail.ru>
2024-07-08 06:14:02 +0300

    Task 2.2.1-4 completed

---
+++ course1/12.package/1.package_module/task1.12.1.2/goget.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#chmod +rwx goget.sh
+
+if [ -z "$1" ]; then
+ echo "Необходимо указать имя модуля"
+ exit 1
+fi
+
+go mod init $1
+go get github.com/yuin/goldmark
+
---
+++ course1/12.package/1.package_module/task1.12.1.3/tidy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#chmod +rwx tidy.sh
+
+if [ -z "$1" ]; then
+ echo "Необходимо указать имя модуля"
+ exit 1
+fi
+
+go mod init $1
+go get github.com/yuin/goldmark
+go mod tidy
+
